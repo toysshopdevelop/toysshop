2d6d5ea4f0c8e2b7e2b88cad229a170ee998b44f
kokosko <kostya.kokosko@gmail.com>
2015-04-29 14:52:02 +0300

    add validation to products and categories forms [issue7]

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
-
 class Product < ActiveRecord::Base
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
   mount_uploader :image_name, ProductUploader
 
