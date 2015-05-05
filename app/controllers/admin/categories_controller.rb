403ff346b64e6b00e6c8018c150075f293c60738
kokosko <kostya.kokosko@gmail.com>
2015-05-05 12:48:36 +0300

    Add select_box to categories form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -15,6 +15,7 @@ class Admin::CategoriesController < ApplicationController
   # GET /categories/new
   def new
     @category = Category.new
+    @categories = Category.all
   end
 
   # GET /categories/1/edit
@@ -65,6 +66,7 @@ class Admin::CategoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
+      @categories = Category.all
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
