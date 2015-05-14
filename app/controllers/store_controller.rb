79c5fe6bbad7d3fa3820d3e1abcd2d52fb41025a
kokosko <kostya.kokosko@gmail.com>
2015-05-14 18:45:28 +0300

    fixnavbars, pagination

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,7 @@ class StoreController < ApplicationController
   layout 'store'
 
   def index
-    @products = Product.order(created_at: :desc).take(50)
+    @products = Product.order(created_at: :desc)
+                .page(params[:page]).per(50)
   end
 end
