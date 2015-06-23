70fdf09101bdf0dacff98d7f8bfe4d45d19b5892
Kostya <kokosko@users.noreply.github.com>
2015-06-23 18:26:49 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'toysshop.develop',
+    user_name:            'liuxintoys',
     password:             ENV['GMAIL_PASS'],
     authentication:       'plain',
     enable_starttls_auto: true  }
